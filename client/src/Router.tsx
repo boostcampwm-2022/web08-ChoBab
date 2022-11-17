59f67379c8c25fbe5420b7ab8da62902736fd2cc
jeong57281 <jeong5728@gmail.com>
2022-11-17 19:56:06 +0900

    feat: 전역 스타일 설정 (styled-components)

--- client/src/Router.tsx
+++ client/src/Router.tsx
@@ -1,8 +1,11 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import GlobalStyle from '@styles/GlobalStyle';
 
 function Router() {
   return (
     <BrowserRouter>
+      <GlobalStyle />
+
       <Routes>
         <Route path="/" element={<div /> /* <HomePage /> */} />
         <Route path="/initRoom" element={<div /> /* <InitRoomPage /> */} />
