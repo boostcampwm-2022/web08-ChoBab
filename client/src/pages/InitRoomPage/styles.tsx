206a7dced66641d8c6b65a18399940b78cac4e3c
권유리 <99jiyusu@gmail.com>
2022-11-22 02:28:28 +0900

    feat: 네이버 지도 연동

---
+++ client/src/pages/InitRoomPage/styles.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+// eslint-disable-next-line import/prefer-default-export
+export const InitRoomPageLayout = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
