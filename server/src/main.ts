935707ae4f381a6fb528d71ad4e6c156c86a981a
권유리 <99jiyusu@gmail.com>
2022-11-21 23:53:20 +0900

    feat: API 응답 템플릿 설정

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { TemplateInterceptor } from './common/interceptors/template.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalInterceptors(new TemplateInterceptor());
   await app.listen(3000);
 }
 bootstrap();
