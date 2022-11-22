1f11288d8e56576c3a474080f3f198a9962c52cd
권유리 <99jiyusu@gmail.com>
2022-11-23 01:54:14 +0900

    feat: exception filter 설정(응답 형식 통일)

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,11 @@
+import { AllExceptionsFilter } from './common/filters/exception.filter';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { TemplateInterceptor } from './common/interceptors/template.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new AllExceptionsFilter());
   app.useGlobalInterceptors(new TemplateInterceptor());
   await app.listen(3000);
 }
