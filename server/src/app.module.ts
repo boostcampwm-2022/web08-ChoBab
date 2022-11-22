cc13f1ba64cd80c5cc166167db7bf448d7b143ea
CodeDiary18 <codediary18@gmail.com>
2022-11-22 16:29:08 +0900

    feat: 환경변수 설정 및 검증

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,7 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+      validationSchema: Joi.object({
+        MONGODB_USERNAME: Joi.string().required(),
+        MONGODB_PASSWORD: Joi.string().required(),
+      }),
+    }),
+  ],
   controllers: [],
   providers: [],
 })
