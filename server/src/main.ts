f8f447f24d3e78c2e1fc1f9cbed1712cc5aa5370
권유리 <99jiyusu@gmail.com>
2022-11-23 02:05:07 +0900

    chore: 백엔드 common 폴더 path alias 설정 및 적용

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,7 +1,7 @@
-import { AllExceptionsFilter } from './common/filters/exception.filter';
+import { AllExceptionsFilter } from '@common/filters/exception.filter';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { TemplateInterceptor } from './common/interceptors/template.interceptor';
+import { TemplateInterceptor } from '@common/interceptors/template.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
