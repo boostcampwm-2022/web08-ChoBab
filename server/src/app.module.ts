b2f232a19b3d8a33e39a9155f762ce4563bb4713
CodeDiary18 <codediary18@gmail.com>
2022-11-23 00:28:11 +0900

    refactor: 데이터 베이스 이름 환경 변수 사용

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -12,14 +12,16 @@ import * as Joi from 'joi';
       validationSchema: Joi.object({
         MONGODB_USERNAME: Joi.string().required(),
         MONGODB_PASSWORD: Joi.string().required(),
+        MONGODB_DB_NAME: Joi.string().required(),
       }),
     }),
     MongooseModule.forRootAsync({
       useFactory: async (configService: ConfigService) => ({
         uri:
-          `mongodb+srv://${configService.get('MONGODB_USERNAME')}` +
-          `:${configService.get('MONGODB_PASSWORD')}` +
-          `@chobab.opfwdho.mongodb.net/?retryWrites=true&w=majority`,
+          `mongodb+srv://` +
+          `${configService.get('MONGODB_USERNAME')}:${configService.get('MONGODB_PASSWORD')}` +
+          `@chobab.opfwdho.mongodb.net/` +
+          `${configService.get('MONGODB_DB_NAME')}?retryWrites=true&w=majority`,
       }),
       inject: [ConfigService],
     }),
