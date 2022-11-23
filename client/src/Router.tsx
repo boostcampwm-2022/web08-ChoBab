36e470063349f863c72afe054ed308c815ba53a2
jeong57281 <jeong5728@gmail.com>
2022-11-23 21:07:04 +0900

    feat: 소켓 연결을 테스트하기 위한 room 페이지 임시 생성

--- client/src/Router.tsx
+++ client/src/Router.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import GlobalStyle, { MainLayout } from '@styles/GlobalStyle';
 import HomePage from '@pages/HomePage';
 import InitRoomPage from '@pages/InitRoomPage';
+import RoomPage from '@pages/RoomPage';
 
 function Router() {
   return (
@@ -12,6 +13,7 @@ function Router() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/init-room" element={<InitRoomPage />} />
+          <Route path="/room/:id" element={<RoomPage />} />
         </Routes>
       </MainLayout>
     </BrowserRouter>
---
+++ client/src/pages/RoomPage/index.tsx
@@ -0,0 +1,63 @@
+import { useState, useEffect } from 'react';
+import { useSocket } from '@hooks/useSocket';
+
+function RoomPage() {
+  const [ready, setReady] = useState<boolean>(false);
+
+  const [socket, connectSocket, disconnectSocket] = useSocket();
+
+  const loading = async () => {
+    try {
+      // TODO: 소켓연결 작업뿐만이 아닌 다른 로딩관련 작업 추가
+      await connectSocket();
+
+      setReady(true);
+    } catch (error) {
+      // TODO: 오류 페이지로 이동관련 로직 추가
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    loading();
+
+    return () => {
+      disconnectSocket();
+    };
+  }, []);
+
+  useEffect(() => {
+    if (socket.current === null) {
+      return;
+    }
+
+    if (!ready) {
+      return;
+    }
+
+    socket.current.on('serverToClient', (data) => {
+      console.log('from server', data);
+    });
+  }, [ready]);
+
+  const sendMessage = () => {
+    if (socket.current === null) {
+      return;
+    }
+
+    socket.current.emit('clientToServer', 'message');
+  };
+
+  return !ready ? (
+    <div>loading...</div>
+  ) : (
+    <div>
+      room page
+      <button type="button" onClick={sendMessage}>
+        message send
+      </button>
+    </div>
+  );
+}
+
+export default RoomPage;
