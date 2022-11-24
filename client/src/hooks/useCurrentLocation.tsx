f71ca6bb172af471f4362ce1cf803db650a0fb79
권유리 <99jiyusu@gmail.com>
2022-11-24 12:13:28 +0900

    feat: 내 위치 받아오기 custom hook 작성

---
+++ client/src/hooks/useCurrentLocation.tsx
@@ -0,0 +1,33 @@
+import { useState, useEffect } from 'react';
+import { NAVER_LAT, NAVER_LNG } from '@constants/map';
+
+interface locationType {
+  lat: number;
+  lng: number;
+}
+
+const useCurrentLocation = () => {
+  const [location, setLocation] = useState<locationType>();
+
+  const handleSuccess = (position: GeolocationPosition) => {
+    setLocation({ lat: position.coords.latitude, lng: position.coords.longitude });
+  };
+
+  const handleError = () => {
+    setLocation({ lat: NAVER_LAT, lng: NAVER_LNG });
+  };
+
+  useEffect(() => {
+    // 위치 사용 불가 장치인 경우
+    if (!('geolocation' in navigator)) {
+      handleError();
+      return;
+    }
+
+    navigator.geolocation.getCurrentPosition(handleSuccess, handleError);
+  }, []);
+
+  return location;
+};
+
+export default useCurrentLocation;
