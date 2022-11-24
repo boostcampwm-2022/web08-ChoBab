ca4ea48c0b409eda435a2d436c4029ad5e32b202
권유리 <99jiyusu@gmail.com>
2022-11-24 20:24:07 +0900

    feat: InitRoomPage footer 생성

--- client/src/pages/InitRoomPage/index.tsx
+++ client/src/pages/InitRoomPage/index.tsx
@@ -1,11 +1,18 @@
-import React, { useEffect, useRef } from 'react';
-import { InitRoomPageLayout, MarkerBox } from '@pages/InitRoomPage/styles';
+import React, { useEffect, useRef, useState } from 'react';
+import {
+  InitRoomPageLayout,
+  MarkerBox,
+  MapBox,
+  FooterBox,
+  StartButton,
+} from '@pages/InitRoomPage/styles';
 import useCurrentLocation from '@hooks/useCurrentLocation';
 import { ReactComponent as MarkerImage } from '@assets/images/marker.svg';
 
 function InitRoomPage() {
   const mapRef = useRef<HTMLDivElement>(null);
   const location = useCurrentLocation();
+  const [address, setAddress] = useState<string>('주소 초기값');
 
   useEffect(() => {
     if (!mapRef.current) {
@@ -27,6 +34,8 @@ function InitRoomPage() {
       const dragEndListener = naver.maps.Event.addListener(map, 'dragend', () => {
         console.log(map?.getCenter().x); // lng
         console.log(map?.getCenter().y); // lat
+        // TODO: 수정 필요한 부분
+        setAddress('주소 설정');
       });
 
       return dragEndListener;
@@ -42,10 +51,21 @@ function InitRoomPage() {
   }, [location]);
 
   return (
-    <InitRoomPageLayout ref={mapRef}>
-      <MarkerBox>
-        <MarkerImage />
-      </MarkerBox>
+    <InitRoomPageLayout>
+      <MapBox ref={mapRef}>
+        <MarkerBox>
+          <MarkerImage />
+        </MarkerBox>
+      </MapBox>
+      <FooterBox>
+        <span>
+          <p>모임 위치를 정해주세요!</p>
+          <p>(추후 수정 가능합니다.)</p>
+        </span>
+        <div>검색창 영역</div>
+        <div>{address}</div>
+        <StartButton title="시작하기">시작하기</StartButton>
+      </FooterBox>
     </InitRoomPageLayout>
   );
 }
