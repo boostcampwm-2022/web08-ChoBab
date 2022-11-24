2b7f01de767b66e9a2f78b630eb279c68e7066d5
권유리 <99jiyusu@gmail.com>
2022-11-24 18:30:31 +0900

    feat: 지도 드래그 시 마커 고정 및 중심 위치 받기

--- client/src/pages/InitRoomPage/index.tsx
+++ client/src/pages/InitRoomPage/index.tsx
@@ -1,33 +1,53 @@
-import { useEffect, useRef, useState } from 'react';
-import { InitRoomPageLayout } from '@pages/InitRoomPage/styles';
+import React, { useEffect, useRef } from 'react';
+import { InitRoomPageLayout, MarkerBox } from '@pages/InitRoomPage/styles';
 import useCurrentLocation from '@hooks/useCurrentLocation';
+import { ReactComponent as MarkerImage } from '@assets/images/marker.svg';
 
 function InitRoomPage() {
   const mapRef = useRef<HTMLDivElement>(null);
   const location = useCurrentLocation();
 
-  // 현재 위치에 맞춰 지도 생성
   useEffect(() => {
-    const initMap = () => {
-      if (!mapRef.current) {
-        return;
-      }
+    if (!mapRef.current) {
+      return;
+    }
 
-      const map = new naver.maps.Map(mapRef.current, {
+    // 현재 위치에 맞춰 지도 생성
+    const createMap = (targetDiv: HTMLDivElement) => {
+      const map = new naver.maps.Map(targetDiv, {
         center: new naver.maps.LatLng(location.lat, location.lng),
         zoom: 14,
       });
 
-      const marker = new naver.maps.Marker({
-        position: new naver.maps.LatLng(location.lat, location.lng),
-        map,
-      });
+      return map;
     };
 
-    initMap();
+    // dragEnd 이벤트 핸들러 생성
+    const onDragEnd = (map: naver.maps.Map): naver.maps.MapEventListener => {
+      const dragEndListener = naver.maps.Event.addListener(map, 'dragend', () => {
+        console.log(map?.getCenter().x); // lng
+        console.log(map?.getCenter().y); // lat
+      });
+
+      return dragEndListener;
+    };
+
+    const map = createMap(mapRef.current);
+    const dragEndListener = onDragEnd(map);
+
+    // eslint-disable-next-line consistent-return
+    return () => {
+      naver.maps.Event.removeListener(dragEndListener);
+    };
   }, [location]);
 
-  return <InitRoomPageLayout ref={mapRef} />;
+  return (
+    <InitRoomPageLayout ref={mapRef}>
+      <MarkerBox>
+        <MarkerImage />
+      </MarkerBox>
+    </InitRoomPageLayout>
+  );
 }
 
 export default InitRoomPage;
--- client/src/pages/InitRoomPage/styles.tsx
+++ client/src/pages/InitRoomPage/styles.tsx
@@ -9,4 +9,12 @@ export const InitRoomPageLayout = styled.div`
   &:focus-visible {
     outline: none;
   }
+  z-index: 1;
+`;
+
+export const MarkerBox = styled.div`
+  z-index: 2;
+  /* margin 들어간 이유: 현재 깃발 마커 이미지 크기에 따라 중심 위치를 네이버 마커와 맞추기 위해 */
+  margin-left: 26px;
+  margin-bottom: 34px;
 `;
