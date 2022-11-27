169e72af836cef0cd3e89a2be951039b01a9ef8f
One-armed-boy <fanatic5500@gmail.com>
2022-11-27 17:09:23 +0900

    feat: roomCode params를 이용한 main page로의 동적 라우팅 구현

--- client/src/Router.tsx
+++ client/src/Router.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import GlobalStyle, { MainLayout } from '@styles/GlobalStyle';
 import HomePage from '@pages/HomePage';
 import InitRoomPage from '@pages/InitRoomPage';
+import { MainRoomPage } from '@pages/MainRoomPage';
 
 function Router() {
   return (
@@ -12,6 +13,7 @@ function Router() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/init-room" element={<InitRoomPage />} />
+          <Route path="/room/:roomCode" element={<MainRoomPage/>}/>
         </Routes>
       </MainLayout>
     </BrowserRouter>
