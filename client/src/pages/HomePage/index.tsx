e1ae2750daa98a3b0862b88eb8e029ace8e038ce
One-armed-boy <fanatic5500@gmail.com>
2022-11-27 17:14:19 +0900

    feat: 초기 화면에서 모임 참여 버튼 클릭 시 등장하는 roomCode 입력 모달 구현

--- client/src/pages/HomePage/index.tsx
+++ client/src/pages/HomePage/index.tsx
@@ -1,8 +1,49 @@
 import { ReactComponent as LogoImage } from '@assets/images/logo.svg';
-import { Link } from 'react-router-dom';
-import { HomePageLayout, HomePageBox, Button, FooterBox, ButtonBox } from './styles';
+import { Link, useNavigate } from 'react-router-dom';
+import { useEffect, useRef, useState } from 'react';
+import {
+  HomePageLayout,
+  HomePageBox,
+  Button,
+  FooterBox,
+  ButtonBox,
+  ModalLayout,
+  ModalBox,
+  InputButton,
+} from './styles';
+
+function Modal() {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const navigate = useNavigate();
+  return (
+    <ModalBox>
+      <form>
+        <input ref={inputRef} placeholder="모임 코드를 입력하세요." />
+        <InputButton
+          onClick={(e) => {
+            e.preventDefault();
+            if (!inputRef.current) {
+              return;
+            }
+            navigate(`/room/${inputRef.current.value}`);
+          }}
+        >
+          입장
+        </InputButton>
+      </form>
+    </ModalBox>
+  );
+}
 
 function HomePage() {
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const modalRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    if (!modalRef.current) {
+      return;
+    }
+    modalRef.current.style.visibility = isModalOpen ? 'visible' : 'hidden';
+  }, [isModalOpen]);
   return (
     <HomePageLayout>
       <HomePageBox>
@@ -12,15 +53,24 @@ function HomePage() {
           <Link to="/init-room">
             <Button>모임 생성</Button>
           </Link>
-          <Link to="/join-room">
-            <Button>모임 참여</Button>
-          </Link>
+          <Button onClick={() => setIsModalOpen(!isModalOpen)}>모임 참여</Button>
         </ButtonBox>
       </HomePageBox>
       <FooterBox>
         <p>밥을 골라주마.</p>
         <p>Copyright 2022. Chobab. All Right Reserved.</p>
       </FooterBox>
+      <ModalLayout
+        ref={modalRef}
+        onClick={(e) => {
+          if (!modalRef.current || modalRef.current !== e.target) {
+            return;
+          }
+          setIsModalOpen(!isModalOpen);
+        }}
+      >
+        <Modal />
+      </ModalLayout>
     </HomePageLayout>
   );
 }
