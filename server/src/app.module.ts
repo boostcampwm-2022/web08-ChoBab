3748ca8893f8ec7a89bf1d0bf1a59dc1d4b2cf5d
CodeDiary18 <codediary18@gmail.com>
2022-11-29 02:40:41 +0900

    refactor: socket cookie 환경 변수 사용

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -17,6 +17,7 @@ import { EventsGateway } from '@socket/socket.gateway';
         MONGODB_PASSWORD: Joi.string().required(),
         MONGODB_DB_NAME: Joi.string().required(),
         KAKAO_API_KEY: Joi.string().required(),
+        COOKIE_SECRET: Joi.string().required(),
       }),
     }),
     MongooseModule.forRootAsync({
--- server/src/socket/socket.gateway.ts
+++ server/src/socket/socket.gateway.ts
@@ -10,19 +10,7 @@ import {
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 import * as session from 'express-session';
-
-const sessionMiddleware = session({
-  resave: false,
-  saveUninitialized: true,
-  secret: 'cookie-secret',
-  cookie: {
-    httpOnly: true,
-    secure: false,
-    // expires 를 따로 설정해주지 않으면 브라우저가 닫혔을 때 세션쿠키가 삭제되기 때문에
-    // 적당한 세션쿠키 유효시간을 설정해 줌 (1시간)
-    expires: new Date(Date.now() + 1000 * 60 * 60),
-  },
-});
+import { ConfigService } from '@nestjs/config';
 
 @WebSocketGateway({ namespace: 'room' })
 export class EventsGateway
@@ -31,11 +19,30 @@ export class EventsGateway
   @WebSocketServer()
   server: Server; // namespace server instance
 
+  private readonly COOKIE_SECRET: string;
+  sessionMiddleware;
+
+  constructor(private configService: ConfigService) {
+    this.COOKIE_SECRET = this.configService.get('COOKIE_SECRET');
+    this.sessionMiddleware = session({
+      resave: false,
+      saveUninitialized: true,
+      secret: this.COOKIE_SECRET,
+      cookie: {
+        httpOnly: true,
+        secure: false,
+        // expires 를 따로 설정해주지 않으면 브라우저가 닫혔을 때 세션쿠키가 삭제되기 때문에
+        // 적당한 세션쿠키 유효시간을 설정해 줌 (1시간)
+        expires: new Date(Date.now() + 1000 * 60 * 60),
+      },
+    });
+  }
+
   onModuleInit() {
     this.server.use((socket, next) => {
       console.log('session middleware');
 
-      sessionMiddleware(socket.request, {}, next);
+      this.sessionMiddleware(socket.request, {}, next);
     });
 
     this.server.use((socket: Socket, next) => {
