af832db1e5666dc480217766d21fe4cedeb1e790
jeong57281 <jeong5728@gmail.com>
2022-11-28 23:40:25 +0900

    feat: 소켓 서버에 세션 미들웨어 장착

--- server/src/socket/socket.gateway.ts
+++ server/src/socket/socket.gateway.ts
@@ -1,3 +1,4 @@
+import { OnModuleInit } from '@nestjs/common';
 import {
   MessageBody,
   WebSocketGateway,
@@ -7,12 +8,57 @@ import {
   OnGatewayConnection,
   OnGatewayDisconnect,
 } from '@nestjs/websockets';
-import { Server } from 'socket.io';
+import { Server, Socket } from 'socket.io';
+import * as session from 'express-session';
+import { Request } from 'express';
+
+declare module 'http' {
+  interface IncomingMessage {
+    session: session & {
+      nickName: string;
+    };
+    sessionID: string;
+  }
+}
+
+const sessionMiddleware = session({
+  resave: false,
+  saveUninitialized: true,
+  secret: 'cookie-secret',
+  cookie: {
+    httpOnly: true,
+    secure: false,
+    // expires 를 따로 설정해주지 않으면 브라우저가 닫혔을 때 세션쿠키가 삭제되기 때문에
+    // 적당한 세션쿠키 유효시간을 설정해 줌 (1시간)
+    expires: new Date(Date.now() + 1000 * 60 * 60),
+  },
+});
 
 @WebSocketGateway({ namespace: 'room' })
-export class EventsGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
+export class EventsGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect, OnModuleInit
+{
   @WebSocketServer()
-  server: Server;
+  server: Server; // namespace server instance
+
+  onModuleInit() {
+    this.server.use((socket, next) => {
+      console.log('session middleware');
+
+      sessionMiddleware(socket.request, {}, next);
+    });
+
+    this.server.use((socket: Socket, next) => {
+      const req = socket.request as Request;
+
+      console.log('session id', req.sessionID);
+      console.log('session data', req.session);
+
+      Object.assign(socket, { sessionID: req.sessionID });
+
+      next();
+    });
+  }
 
   @SubscribeMessage('clientToServer')
   handleClientToServerMessage(@MessageBody() data: any) {
