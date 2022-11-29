95536ab7f4e1812de783d0152757f1b3179e44d3
One-armed-boy <fanatic5500@gmail.com>
2022-11-29 16:58:34 +0900

    feat: 사용자가 모임에 입장한 뒤 서버와 소켓 이벤트를 거쳐 지도 화면이 렌더링 되는 화면 구성

--- client/src/pages/MainPage/index.tsx
+++ client/src/pages/MainPage/index.tsx
@@ -1,76 +1,145 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { Socket } from 'socket.io-client';
 import { useSocket } from '@hooks/useSocket';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
-import { MainPageLayout } from './styles';
+import useCurrentLocation from '@hooks/useCurrentLocation';
+import { HeaderBox, MainPageLayout, MapBox, CategoryToggle, Header } from './styles';
+
+interface RestaurantType {
+  id: string;
+  name: string;
+  category: string;
+  phone: string;
+  lat: number;
+  lng: number;
+  address: string;
+}
+
+interface RoomValidResponseType {
+  message: string;
+  data: {
+    isRoomValid: boolean;
+  };
+}
+
+interface UserType {
+  userId: string;
+  userLat: number;
+  userLng: number;
+}
 
 function MainPage() {
+  const mapRef = useRef<HTMLDivElement>(null);
+  const userLocation = useCurrentLocation();
   const { roomCode } = useParams<{ roomCode: string }>();
-  const [isReady, setIsReady] = useState<boolean>(false);
-
+  const [isRoomConnect, setRoomConnect] = useState<boolean>(false);
   const [socketRef, connectSocket, disconnectSocket] = useSocket();
 
-  const initService = async () => {
-    try {
-      await connectSocket();
-      const res = await axios.post('/api/room/connect', {
-        roomCode,
-        // 향후 유저 식별자가 userId에 추가되어야 함
-        userId: '이창명4',
-      });
+  const [restaurantData, setRestaurantData] = useState<RestaurantType[]>([]);
+  const [roomLocation, setRoomLocation] = useState<{ lat: number | null; lng: number | null }>({
+    lat: null,
+    lng: null,
+  });
 
-      if (res.status < 300) {
-        setIsReady(true);
-        console.log(res.data);
+  useEffect(() => {
+    const { lat: userLat, lng: userLng } = userLocation;
+    if (!userLat || !userLng) {
+      return;
+    }
+    if (!roomCode) {
+      return;
+    }
+    if (isRoomConnect) {
+      return;
+    }
+    const initService = async () => {
+      const connectRoom = (userId: string) => {
+        const clientSocket = socketRef.current;
+        if (!(clientSocket instanceof Socket)) {
+          throw new Error();
+        }
+        clientSocket.on(
+          'connectResult',
+          (data: {
+            message: string;
+            data?: {
+              roomCode: string;
+              lat: number;
+              lng: number;
+              userList: UserType[];
+              restaurantList: RestaurantType[];
+              candidateList: RestaurantType[];
+            };
+          }) => {
+            if (!data.data) {
+              console.log(data.message);
+              return;
+            }
+            const { lat, lng, userList, restaurantList, candidateList } = data.data;
+            setRoomConnect(true);
+            setRestaurantData(restaurantList);
+            setRoomLocation({ ...roomLocation, ...{ lat, lng } });
+            console.log(data);
+          }
+        );
+        clientSocket.emit('connectRoom', { roomCode, userId, userLat, userLng });
+      };
+      try {
+        await connectSocket();
+        const {
+          data: {
+            data: { isRoomValid },
+          },
+        } = await axios.get<RoomValidResponseType>(`/api/room/valid?roomCode=${roomCode}`);
+
+        if (!isRoomValid) {
+          throw new Error('입장하고자 하는 방이 올바르지 않습니다.');
+        }
+
+        connectRoom('창명');
+      } catch (error) {
+        console.log(error);
       }
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  useEffect(() => {
-    initService();
-
-    return () => {
-      disconnectSocket();
     };
-  }, []);
+
+    initService();
+    return () => { // eslint-disable-line consistent-return
+      disconnectSocket();  
+    };
+  }, [userLocation]);
 
   useEffect(() => {
-    const socket = socketRef.current;
-
-    if (!(socket instanceof Socket)) {
+    if (!isRoomConnect) {
       return;
     }
-
-    if (!isReady) {
+    if (!mapRef.current) {
       return;
     }
-
-    socket.on('serverToClient', (data) => {
-      console.log('from server', data);
-    });
-  }, [isReady]);
-
-  const sendMessage = () => {
-    const socket = socketRef.current;
-
-    if (!(socket instanceof Socket)) {
+    if (!roomLocation.lat || !roomLocation.lng) {
       return;
     }
+    const initMap = () => {
+      if (!mapRef.current || !roomLocation.lat || !roomLocation.lng) {
+        return;
+      }
+      const map = new naver.maps.Map(mapRef.current, {
+        center: new naver.maps.LatLng(roomLocation.lat, roomLocation.lng),
+        zoom: 14,
+      });
+    };
+    initMap();
+  }, [isRoomConnect]);
 
-    socket.emit('clientToServer', 'message');
-  };
-
-  return !isReady ? (
+  return !isRoomConnect ? (
     <div>loading...</div>
   ) : (
     <MainPageLayout>
-      room page
-      <button type="button" onClick={sendMessage}>
-        message send
-      </button>
+      <MapBox ref={mapRef} />
+      <HeaderBox>
+        <Header>헤더</Header>
+        <CategoryToggle>토글</CategoryToggle>
+      </HeaderBox>
     </MainPageLayout>
   );
 }
