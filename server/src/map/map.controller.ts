0900d283281687dbae82419221c68b2b6fb6dbaf
CodeDiary18 <codediary18@gmail.com>
2022-11-30 02:56:38 +0900

    chore: map 도메인 controller, service 기본 생성

---
+++ server/src/map/map.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('map')
+export class MapController {}
--- server/src/map/map.module.ts
+++ server/src/map/map.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { MapController } from './map.controller';
+import { MapService } from './map.service';
 
-@Module({})
+@Module({
+  controllers: [MapController],
+  providers: [MapService],
+})
 export class MapModule {}
