f357089155517ea97b29f099c3dab67bc4836d20
권유리 <99jiyusu@gmail.com>
2022-11-29 14:05:37 +0900

    refactor: useEffect에서 함수 선언부 분리

--- client/src/pages/InitRoomPage/index.tsx
+++ client/src/pages/InitRoomPage/index.tsx
@@ -15,63 +15,63 @@ function InitRoomPage() {
   const userLocation = useCurrentLocation();
   const [address, setAddress] = useState<string>(NAVER_ADDRESS);
 
+  // 좌표 -> 주소 변환 & setAddress
+  const updateAddress = (lat: number, lng: number) => {
+    naver.maps.Service.reverseGeocode(
+      {
+        coords: new naver.maps.LatLng(lat, lng),
+        orders: [naver.maps.Service.OrderType.ROAD_ADDR, naver.maps.Service.OrderType.ADDR].join(
+          ','
+        ),
+      },
+      // eslint-disable-next-line consistent-return
+      (status, response) => {
+        if (status !== naver.maps.Service.Status.OK) {
+          alert('주소 변환 실패');
+        }
+        setAddress(response.v2.address.roadAddress || response.v2.address.jibunAddress);
+      }
+    );
+  };
+
+  // 현재 위치에 맞춰 지도 생성
+  const createMap = (targetDiv: HTMLDivElement): naver.maps.Map => {
+    const map = new naver.maps.Map(targetDiv, {
+      center: new naver.maps.LatLng(userLocation.lat, userLocation.lng),
+      zoom: 14,
+    });
+
+    return map;
+  };
+
+  // dragEnd 이벤트 핸들러 생성
+  const onDragEnd = (map: naver.maps.Map): naver.maps.MapEventListener => {
+    const dragEndListener = naver.maps.Event.addListener(map, 'dragend', async () => {
+      const lng = map.getCenter().x;
+      const lat = map.getCenter().y;
+
+      updateAddress(lat, lng);
+    });
+
+    return dragEndListener;
+  };
+
+  // zoom_changed 이벤트 핸들러 생성
+  const onZoomChanged = (map: naver.maps.Map): naver.maps.MapEventListener => {
+    const zoomChangedListener = naver.maps.Event.addListener(map, 'zoom_changed', async () => {
+      const lng = map.getCenter().x;
+      const lat = map.getCenter().y;
+
+      updateAddress(lat, lng);
+    });
+
+    return zoomChangedListener;
+  };
+
   useEffect(() => {
     if (!mapRef.current) {
       return;
     }
-
-    // 좌표 -> 주소 변환 & setAddress
-    const updateAddress = (lat: number, lng: number) => {
-      naver.maps.Service.reverseGeocode(
-        {
-          coords: new naver.maps.LatLng(lat, lng),
-          orders: [naver.maps.Service.OrderType.ROAD_ADDR, naver.maps.Service.OrderType.ADDR].join(
-            ','
-          ),
-        },
-        // eslint-disable-next-line consistent-return
-        (status, response) => {
-          if (status !== naver.maps.Service.Status.OK) {
-            alert('주소 변환 실패');
-          }
-          setAddress(response.v2.address.roadAddress || response.v2.address.jibunAddress);
-        }
-      );
-    };
-
-    // 현재 위치에 맞춰 지도 생성
-    const createMap = (targetDiv: HTMLDivElement): naver.maps.Map => {
-      const map = new naver.maps.Map(targetDiv, {
-        center: new naver.maps.LatLng(userLocation.lat, userLocation.lng),
-        zoom: 14,
-      });
-
-      return map;
-    };
-
-    // dragEnd 이벤트 핸들러 생성
-    const onDragEnd = (map: naver.maps.Map): naver.maps.MapEventListener => {
-      const dragEndListener = naver.maps.Event.addListener(map, 'dragend', async () => {
-        const lng = map.getCenter().x;
-        const lat = map.getCenter().y;
-
-        updateAddress(lat, lng);
-      });
-
-      return dragEndListener;
-    };
-
-    // zoom_changed 이벤트 핸들러 생성
-    const onZoomChanged = (map: naver.maps.Map): naver.maps.MapEventListener => {
-      const zoomChangedListener = naver.maps.Event.addListener(map, 'zoom_changed', async () => {
-        const lng = map.getCenter().x;
-        const lat = map.getCenter().y;
-
-        updateAddress(lat, lng);
-      });
-
-      return zoomChangedListener;
-    };
 
     // init
     const map = createMap(mapRef.current);
