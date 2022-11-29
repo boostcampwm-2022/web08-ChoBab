526e0579d7907257dc9eeab91b34d32f54ee06f6
One-armed-boy <fanatic5500@gmail.com>
2022-11-29 16:32:04 +0900

    feat: 소켓을 통해 사용자가 room 에 입장하는 로직 작성

--- server/src/socket/socket.gateway.ts
+++ server/src/socket/socket.gateway.ts
@@ -1,3 +1,4 @@
+import { InjectModel } from '@nestjs/mongoose';
 import {
   MessageBody,
   WebSocketGateway,
@@ -6,11 +7,18 @@ import {
   OnGatewayInit,
   OnGatewayConnection,
   OnGatewayDisconnect,
+  ConnectedSocket,
 } from '@nestjs/websockets';
-import { Server } from 'socket.io';
+import { Room, RoomDocument, RoomDynamic, RoomDynamicDocument } from '@room/room.schema';
+import { Model } from 'mongoose';
+import { Server, Socket } from 'socket.io';
 
 @WebSocketGateway({ namespace: 'room' })
 export class EventsGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
+  constructor(
+    @InjectModel(RoomDynamic.name) private roomDynamicModel: Model<RoomDynamicDocument>,
+    @InjectModel(Room.name) private roomModel: Model<RoomDocument>
+  ) {}
   @WebSocketServer()
   server: Server;
 
@@ -18,6 +26,47 @@ export class EventsGateway implements OnGatewayInit, OnGatewayConnection, OnGate
   handleClientToServerMessage(@MessageBody() data: any) {
     console.log('from client', data);
     this.server.emit('serverToClient', data);
+  }
+
+  @SubscribeMessage('connectRoom')
+  async handleConnectRoom(
+    @ConnectedSocket() client: Socket,
+    @MessageBody('userId') userId: string,
+    @MessageBody('roomCode') roomCode: string,
+    @MessageBody('userLat') userLat: number,
+    @MessageBody('userLng') userLng: number
+  ) {
+    const user = { userId, userLat, userLng };
+    console.log(userId, roomCode);
+    client.join(roomCode);
+    try {
+      const { lat, lng } = await this.roomModel.findOne({ roomCode });
+      const { restaurantList, userList, candidateList } = await this.roomDynamicModel.findOne({
+        roomCode,
+      });
+      if (userList.filter((userData) => userData.userId === userId).length > 0) {
+        client.emit('connectResult', {
+          message: '이미 접속 중인 사용자입니다.',
+          data: { roomCode, lat, lng, restaurantList, candidateList, userList },
+        });
+        return;
+      }
+
+      const newUserList = [...userList, user];
+      await this.roomDynamicModel.findOneAndUpdate(
+        { roomCode },
+        {
+          userList: newUserList,
+        }
+      );
+
+      client.emit('connectResult', {
+        message: '입장 성공',
+        data: { roomCode, lat, lng, restaurantList, candidateList, userList: newUserList },
+      });
+    } catch (error) {
+      client.emit('connectResult', { message: '방 접속에 실패했습니다.' });
+    }
   }
 
   afterInit() {
