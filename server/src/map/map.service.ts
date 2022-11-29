229f34610eb3b0781f3dd9f92b816bcc88b6cc38
CodeDiary18 <codediary18@gmail.com>
2022-11-30 03:19:34 +0900

    chore: map service에서 환경 변수 로드

--- server/src/map/map.service.ts
+++ server/src/map/map.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
-export class MapService {}
+export class MapService {
+  private readonly API_CLIENT_ID: string;
+  private readonly API_CLIENT_SECRET: string;
+
+  constructor(private readonly configService: ConfigService) {
+    this.API_CLIENT_ID = configService.get('NAVER_MAP_API_CLIENT_ID');
+    this.API_CLIENT_SECRET = configService.get('NAVER_MAP_API_CLIENT_SECRET');
+  }
+}
