adc8eeee00dc4ef8d700d0817c06a7a52a9f1b5b
권유리 <99jiyusu@gmail.com>
2022-11-29 14:09:57 +0900

    refactor: 불필요한 async 키워드 삭제

--- client/src/pages/InitRoomPage/index.tsx
+++ client/src/pages/InitRoomPage/index.tsx
@@ -46,7 +46,7 @@ function InitRoomPage() {
 
   // dragEnd 이벤트 핸들러 생성
   const onDragEnd = (map: naver.maps.Map): naver.maps.MapEventListener => {
-    const dragEndListener = naver.maps.Event.addListener(map, 'dragend', async () => {
+    const dragEndListener = naver.maps.Event.addListener(map, 'dragend', () => {
       const lng = map.getCenter().x;
       const lat = map.getCenter().y;
 
@@ -58,7 +58,7 @@ function InitRoomPage() {
 
   // zoom_changed 이벤트 핸들러 생성
   const onZoomChanged = (map: naver.maps.Map): naver.maps.MapEventListener => {
-    const zoomChangedListener = naver.maps.Event.addListener(map, 'zoom_changed', async () => {
+    const zoomChangedListener = naver.maps.Event.addListener(map, 'zoom_changed', () => {
       const lng = map.getCenter().x;
       const lat = map.getCenter().y;
 
