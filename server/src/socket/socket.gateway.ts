9743d96b5b6c6a3bf377a7e71548f9a7639b143d
jeong57281 <jeong5728@gmail.com>
2022-12-02 03:09:30 +0900

    feat: 세션 관련 작업
    - 방 접속 시
      - userId, userName(random 생성) 추가로 전달
      - 접속을 모든 클라이언트에게 알림
    - 방 퇴장 시 같은 세션 접속자가 없으면 db 와 클라이언트에 퇴장 반영
    - socket 에 roomCode 데이터 저장

--- server/src/socket/socket.gateway.ts
+++ server/src/socket/socket.gateway.ts
@@ -27,6 +27,9 @@ interface UserType {
 export class EventsGateway
   implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect, OnModuleInit
 {
+  @WebSocketServer()
+  server: Server; // 'room' namespace server instance
+
   constructor(
     @InjectModel(RoomDynamic.name) private roomDynamicModel: Model<RoomDynamicDocument>,
     @InjectModel(Room.name) private roomModel: Model<RoomDocument>
@@ -50,14 +53,29 @@ export class EventsGateway
         lng: number,
         restaurantList: RestaurantType[],
         candidateList: RestaurantType[],
-        userList: UserType[]
+        userList: UserType[],
+        userId: string,
+        userName: string
       ) =>
-        dataTemplate('접속 성공', { roomCode, lat, lng, restaurantList, candidateList, userList }),
+        dataTemplate('접속 성공', {
+          roomCode,
+          lat,
+          lng,
+          restaurantList,
+          candidateList,
+          userList,
+          userId,
+          userName,
+        }),
     };
   }
 
-  @WebSocketServer()
-  server: Server; // namespace server instance
+  private createRandomNickname() {
+    const firstName = ['박', '이', '권', '강'];
+    const LastName = ['정현', '창명', '유리', '윤희'];
+
+    return firstName[Math.floor(Math.random() * 4)] + LastName[Math.floor(Math.random() * 4)];
+  }
 
   onModuleInit() {
     this.server.use((socket, next) => {
@@ -67,24 +85,10 @@ export class EventsGateway
     this.server.use((socket: Socket, next) => {
       const req = socket.request;
 
-      console.log('session id', req.sessionID);
-      console.log('session data', req.session);
-
       Object.assign(socket, { sessionID: req.sessionID });
 
       next();
     });
-  }
-
-  @SubscribeMessage('clientToServer')
-  handleClientToServerMessage(client: Socket, data: any) {
-    const req = client.request;
-
-    console.log('session id', req.sessionID);
-    console.log('session data', req.session);
-
-    console.log('from client', data);
-    this.server.emit('serverToClient', data);
   }
 
   @SubscribeMessage('connectRoom')
@@ -94,36 +98,51 @@ export class EventsGateway
     @MessageBody('userLat') userLat: number,
     @MessageBody('userLng') userLng: number
   ) {
-    const { sessionID: userId } = client.request;
-    const user = { userId, userLat, userLng };
-    const { CONNECT_SUCCESS, CONNECT_FAIL } = this.socketRes();
-    console.log(userId, roomCode);
+    client.roomCode = roomCode;
+
     client.join(roomCode);
+
+    const { CONNECT_SUCCESS, CONNECT_FAIL } = this.socketRes();
+
     try {
       const { lat, lng } = await this.roomModel.findOne({ roomCode });
       const { restaurantList, userList, candidateList } = await this.roomDynamicModel.findOne({
         roomCode,
       });
-      if (userList.filter((userData) => userData.userId === userId).length > 0) {
-        client.emit(
-          'connectResult',
-          CONNECT_SUCCESS(roomCode, lat, lng, restaurantList, candidateList, userList)
-        );
-        return;
-      }
 
-      const newUserList = [...userList, user];
-      await this.roomDynamicModel.findOneAndUpdate(
-        { roomCode },
-        {
-          userList: newUserList,
-        }
-      );
+      const { sessionID: userId } = client.request;
+
+      const userIds = userList.map((userData) => userData.userId);
+
+      const userIndex = userIds.indexOf(userId);
+
+      let user;
+      let newUserList;
+
+      if (userIndex !== -1) {
+        user = userList[userIndex];
+        newUserList = userList;
+      } else {
+        user = { userId, userLat, userLng, userName: this.createRandomNickname() };
+        newUserList = [...userList, user];
+        await this.roomDynamicModel.findOneAndUpdate({ roomCode }, { userList: newUserList });
+      }
 
       client.emit(
         'connectResult',
-        CONNECT_SUCCESS(roomCode, lat, lng, restaurantList, candidateList, newUserList)
+        CONNECT_SUCCESS(
+          roomCode,
+          lat,
+          lng,
+          restaurantList,
+          candidateList,
+          newUserList,
+          user.userId,
+          user.userName
+        )
       );
+
+      client.to(roomCode).emit('join', user); // 자신을 제외하네?
     } catch (error) {
       client.emit('connectResult', CONNECT_FAIL);
     }
@@ -137,7 +156,26 @@ export class EventsGateway
     console.log('connected');
   }
 
-  handleDisconnect() {
+  async handleDisconnect(client: Socket) {
+    const { sessionID, roomCode } = client;
+
+    let sessionIDs = [];
+
+    // 아래 조건문이 없으면 Map 이 아닌 Namespace 로 인식해서 전개연산자를 사용할수가 없다.
+    if (this.server.sockets instanceof Map) {
+      sessionIDs = [...this.server.sockets]
+        .filter(([key, value]) => value.roomCode === roomCode)
+        .map(([key, value]) => value.sessionID);
+    }
+
+    if (!sessionIDs.includes(sessionID)) {
+      await this.roomDynamicModel.updateOne(
+        { roomCode: roomCode },
+        { $pull: { userList: { userId: sessionID } } }
+      );
+      client.to(roomCode).emit('leave', sessionID);
+    }
+
     console.log('disconnected');
   }
 }
