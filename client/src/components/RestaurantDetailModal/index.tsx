d205ac1afd2f45e5d7e0552ac73b075dd58b6cf0
One-armed-boy <fanatic5500@gmail.com>
2022-12-02 12:10:27 +0900

    feat: 이미지 캐러셀, 메인 부분의 식당명, 카테고리, 평점, 푸터 부분의 주소 렌더링

--- client/src/components/RestaurantDetailModal/index.tsx
+++ client/src/components/RestaurantDetailModal/index.tsx
@@ -1,9 +1,124 @@
-import React, { useState } from 'react';
-import { ModalLayout } from './styles';
+import React, { useEffect, useRef, useState } from 'react';
+import * as palette from '@styles/Variables';
+import { ReactComponent as BackwardIcon } from '@assets/images/backward-arrow-icon.svg';
+import {
+  ImageCarousel,
+  ModalBody,
+  ModalBox,
+  ModalFooter,
+  ModalLayout,
+  ScrollTest,
+  NameBox,
+  CategoryBox,
+  RatingBox,
+  ModalFooterNav,
+  AddressBox,
+  BackwardButton,
+} from './styles';
+
+interface RestaurantDataType {
+  id: string;
+  name: string;
+  category: string;
+  address: string;
+  openNow?: boolean;
+  phone?: string;
+  lat: number;
+  lng: number;
+  rating?: number;
+}
+
+function RestaurantDetailModalFooter({ address }: { address: string }) {
+  const [isSelectLeft, setSelectLeft] = useState<boolean>(true);
+  const operationInfoButtonRef = useRef<HTMLDivElement>(null);
+  const getDirectionButtonRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const operationInfoButton = operationInfoButtonRef.current;
+    const getDirectionButton = getDirectionButtonRef.current;
+    if (!operationInfoButton || !getDirectionButton) {
+      return;
+    }
+    if (!isSelectLeft) {
+      getDirectionButton.style.color = palette.PRIMARY;
+      operationInfoButton.style.color = 'black';
+      return;
+    }
+    getDirectionButton.style.color = 'black';
+    operationInfoButton.style.color = palette.PRIMARY;
+  }, [isSelectLeft]);
+
+  return (
+    <ModalFooter>
+      <ModalFooterNav
+        onClick={(e) => {
+          if (!(e.target instanceof HTMLDivElement)) {
+            return;
+          }
+          const eventTarget = e.target as HTMLDivElement;
+          if (
+            eventTarget !== operationInfoButtonRef.current &&
+            eventTarget !== getDirectionButtonRef.current
+          ) {
+            return;
+          }
+          setSelectLeft(eventTarget === operationInfoButtonRef.current);
+        }}
+      >
+        <div ref={operationInfoButtonRef}>영업 정보</div>
+        <div ref={getDirectionButtonRef}>길찾기</div>
+      </ModalFooterNav>
+      {isSelectLeft ? (
+        <ScrollTest>
+          <AddressBox>{address}</AddressBox>
+        </ScrollTest>
+      ) : (
+        <ScrollTest>길찾기</ScrollTest>
+      )}
+    </ModalFooter>
+  );
+}
 
 export function RestaurantDetailModal() {
-  const [isReady, setReady] = useState<boolean>(false);
-  return <ModalLayout>
-    {isReady?<div>loading...</div>:<div>완료...</div>}
-  </ModalLayout>;
+  const [restaurantData, setRestaurantData] = useState<RestaurantDataType | null>(null);
+
+  useEffect(() => {
+    setTimeout(() => {
+      const mockRestaurantData = {
+        id: '123456',
+        name: '진우동',
+        category: '일식',
+        address: '경기 성남시 분당구 황새울로335번길 8 덕산빌딩',
+        lat: 0,
+        lng: 0,
+        rating: 4.2,
+      };
+      setRestaurantData({ ...restaurantData, ...mockRestaurantData });
+    }, 3000);
+  }, []);
+
+  return (
+    <ModalLayout>
+      {!restaurantData ? (
+        <div>loading...</div>
+      ) : (
+        <>
+          <BackwardButton>
+            <BackwardIcon />
+          </BackwardButton>
+          <ModalBox>
+            <ImageCarousel>사진</ImageCarousel>
+            <ModalBody>
+              <NameBox>{restaurantData.name}</NameBox>
+              <CategoryBox>{restaurantData.category}</CategoryBox>
+              <RatingBox>
+                {!restaurantData.rating ? '평점 정보 없음' : `평점: ${restaurantData.rating}`}
+              </RatingBox>
+            </ModalBody>
+            <RestaurantDetailModalFooter address={restaurantData.address} />
+          </ModalBox>
+        </>
+      )}
+    </ModalLayout>
+  );
 }
--- client/src/components/RestaurantDetailModal/styles.tsx
+++ client/src/components/RestaurantDetailModal/styles.tsx
@@ -1,12 +1,85 @@
 import styled from 'styled-components';
+import * as palette from '@styles/Variables';
 
 export const ModalLayout = styled.div`
-    width: 100%;
-    height: 100%;
-    position: absolute;
-    visibility: visible;
-    z-index: 100;
-    background-color: red;
+  width: 100%;
+  height: 100%;
+  max-height: 100%;
+  position: absolute;
+  visibility: visible;
+  z-index: 999; // 음식점 상세정보보다 더 상위에 겹쳐질 요소가 없다고 판단
 `;
 
-export const ModalBox = styled.div``;
+export const BackwardButton = styled.div`
+  position: absolute;
+  top:10px;
+  left:10px;
+`;
+
+export const ModalBox = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: white;
+  display: flex;
+  flex-direction: column;
+  overflow: auto;
+  -ms-overflow-style: none;
+  ::-webkit-scrollbar {
+    display: none;
+  }
+`;
+
+export const ImageCarousel = styled.div`
+  width: 100%;
+  height: 45%;
+  flex: 2;
+`;
+
+export const ModalBody = styled.div`
+  width: 100%;
+  height: 20%;
+  display: flex;
+  flex-direction: column;
+  padding: 10px 10px;
+  align-items: center;
+  justify-content: center;
+  gap: 10px;
+  box-shadow: 0px 4px 10px rgba(51, 51, 51, 0.1), 0px 0px 4px rgba(51, 51, 51, 0.05);
+`;
+
+export const NameBox = styled.div`
+  font-size: 20px;
+  font-weight: 700;
+`;
+
+export const CategoryBox = styled.div`
+  font-size: 16px;
+`;
+
+export const RatingBox = styled.div``;
+
+export const ModalFooter = styled.div`
+  width: 100%;
+  height: 35%;
+  padding: 30px;
+`;
+
+export const ModalFooterNav = styled.div`
+  width: 100%;
+  height: 20%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-content: center;
+  border-bottom: 1px solid;
+`;
+
+export const OperationInfoBody = styled.div``;
+
+export const ScrollTest = styled.div`
+  width: 100%;
+  padding: 20px 10px;
+  height: 500px;
+`;
+
+export const AddressBox = styled.div``;
