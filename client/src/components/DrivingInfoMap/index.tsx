df0cf1b40766d840206ee6a09858b528b9814c35
CodeDiary18 <codediary18@gmail.com>
2022-12-05 17:11:53 +0900

    feat: 길찾기 API 적용해서 경로 그리기

--- client/src/components/DrivingInfoMap/index.tsx
+++ client/src/components/DrivingInfoMap/index.tsx
@@ -1,5 +1,6 @@
 import { useCallback, useEffect, useRef } from 'react';
 import { MapBox } from '@components/MainMap/styles';
+import axios from 'axios';
 import { DrivingInfoMapLayout } from './styles';
 
 interface PositionType {
@@ -12,38 +13,84 @@ interface PropsType {
   restaurantPos: PositionType;
 }
 
+interface ResTemplateType<T> {
+  message: string;
+  data: T;
+}
+
+interface DrivingInfoType {
+  start: number[];
+  goal: number[];
+  distance: number;
+  duration: number;
+  tollFare: number;
+  taxiFare: number;
+  fuelPrice: number;
+  path: number[][];
+}
+
 function DrivingInfoMap({ userPos, restaurantPos }: PropsType) {
   const mapRef = useRef<HTMLDivElement>(null);
-  const mapSetting = useCallback(() => {
+
+  // 길찾기 API 호출
+  const getDrivingInfo = async (
+    startPos: PositionType,
+    goalPos: PositionType
+  ): Promise<DrivingInfoType> => {
+    const { lat: startLat, lng: startLng } = startPos;
+    const { lat: goalLat, lng: goalLng } = goalPos;
+    try {
+      const {
+        data: { data: drivingInfo },
+      } = await axios.get<ResTemplateType<DrivingInfoType>>('/api/map/driving', {
+        params: {
+          start: `${startLng},${startLat}`,
+          goal: `${goalLng},${goalLat}`,
+        },
+      });
+      return drivingInfo;
+    } catch (error: any) {
+      console.log(error.response.data.message ?? '길찾기 정보를 불러오는데 실패했습니다.');
+      return {} as DrivingInfoType;
+    }
+  };
+
+  const mapSetting = useCallback(async () => {
     if (!mapRef.current) {
       return;
     }
 
     const map = new naver.maps.Map(mapRef.current, {
-      center: new naver.maps.LatLng(37.3674001, 127.1181196),
-      zoom: 14,
+      center: new naver.maps.LatLng(userPos.lat, userPos.lng),
+      zoom: 11,
     });
 
+    // 길찾기 정보를 받아오는 함수 호출
+    const drivingInfo = await getDrivingInfo(userPos, restaurantPos);
+    if (!drivingInfo) {
+      return;
+    }
+
+    // 경로를 표시할 좌표들 배열
+    const drivingInfoPaths: naver.maps.LatLng[] = drivingInfo.path?.map(
+      (pos: number[]) => new naver.maps.LatLng(pos[1], pos[0])
+    );
+
+    // 경로 그리기
     const polyline = new naver.maps.Polyline({
       map,
-      path: [
-        new naver.maps.LatLng(37.359924641705476, 127.1148204803467),
-        new naver.maps.LatLng(37.36343797188166, 127.11486339569092),
-        new naver.maps.LatLng(37.368520071054576, 127.11473464965819),
-        new naver.maps.LatLng(37.3685882848096, 127.1088123321533),
-        new naver.maps.LatLng(37.37295383612657, 127.10876941680907),
-        new naver.maps.LatLng(37.38001321351567, 127.11851119995116),
-        new naver.maps.LatLng(37.378546827477855, 127.11984157562254),
-        new naver.maps.LatLng(37.376637072444105, 127.12052822113036),
-        new naver.maps.LatLng(37.37530703574853, 127.12190151214598),
-        new naver.maps.LatLng(37.371657839593894, 127.11645126342773),
-        new naver.maps.LatLng(37.36855417793982, 127.1207857131958),
-      ],
+      path: drivingInfoPaths,
+      strokeColor: 'blue', // 선 색
+      strokeLineCap: 'round', // 라인의 끝 모양
+      strokeWeight: 5, // 선 두께
     });
+
+    // 지도 중심을 경로의 중심으로 설정
+    map.setCenter(polyline.getBounds().getCenter());
   }, []);
 
   useEffect(() => {
-    mapSetting();
+    mapSetting().then();
   }, []);
 
   return (
