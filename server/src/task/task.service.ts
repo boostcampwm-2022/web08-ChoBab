77f18787bf1f35f2fe6868d40c203ec823dba923
CodeDiary18 <codediary18@gmail.com>
2022-12-07 03:38:26 +0900

    feat: 모임방 자동 삭제 로직
    1시간에 한번씩 기준 시각 이전에 생성된 방은 모두 삭제 처리

--- server/src/task/task.service.ts
+++ server/src/task/task.service.ts
@@ -1,4 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { Cron, CronExpression } from '@nestjs/schedule';
+import { InjectModel } from '@nestjs/mongoose';
+import { Room, RoomDocument, RoomDynamic, RoomDynamicDocument } from '@room/room.schema';
+import { Model } from 'mongoose';
+import { ONE_DAY_MILLISECOND } from '@constants/time';
 
 @Injectable()
-export class TaskService {}
+export class TaskService {
+  private readonly logger = new Logger(TaskService.name);
+
+  constructor(
+    @InjectModel(Room.name) private roomModel: Model<RoomDocument>,
+    @InjectModel(RoomDynamic.name) private roomDynamicModel: Model<RoomDynamicDocument>
+  ) {}
+
+  /**
+   * 매 시각 정각마다 실행하는 room 삭제 작업
+   */
+  @Cron(CronExpression.EVERY_HOUR)
+  async deleteRoomCron() {
+    this.logger.debug('[Delete Room Cron] Called every hour');
+    const now = new Date();
+
+    // 현재로 부터 1일 전 시간
+    const criteria = new Date(now.getTime() - ONE_DAY_MILLISECOND);
+
+    // 기준 시각 이전에 생성된 모임방(roomCode)에 해당하는 roomDynamic 삭제
+    await this.roomModel.find({ createdAt: { $lt: criteria } }).then(async (rooms) => {
+      await Promise.all(
+        rooms.map(async (room) => {
+          await this.roomDynamicModel.deleteOne({ roomCode: room.roomCode });
+        })
+      );
+    });
+
+    // 기준 시각 이전에 생성된 room soft delete
+    await this.roomModel.updateMany({ createdAt: { $lt: criteria } }, { deletedAt: now });
+  }
+}
