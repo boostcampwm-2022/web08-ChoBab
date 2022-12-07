e9c28cdf0f4025ea857fcaca2244dca422c9de57
One-armed-boy <fanatic5500@gmail.com>
2022-12-07 12:25:53 +0900

    refactor: 쓰이지 않는 구문 정리

--- client/src/components/CandidateListModal/styles.tsx
+++ client/src/components/CandidateListModal/styles.tsx
@@ -1,19 +1,16 @@
 import styled from 'styled-components';
 
 export const CandidateListModalLayout = styled.div`
-  /* position: absolute; */
   width: 100%;
   height: 100%;
   padding: 10% 5%;
   background-color: white;
-  /* z-index: 990; */
   overflow: hidden;
 `;
 
 export const CandidateListModalBox = styled.div`
   width: 100%;
   height: 100%;
-  //overflow-y: auto;
   display: flex;
   flex-direction: column;
   gap: 20px;
