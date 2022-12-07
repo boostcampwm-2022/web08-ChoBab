021b3d34d3d0f73cf51d4e739fd416ba7d7708f4
CodeDiary18 <codediary18@gmail.com>
2022-12-07 23:47:38 +0900

    chore: test 페이지 삭제

--- client/src/Router.tsx
+++ client/src/Router.tsx
@@ -1,9 +1,8 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import GlobalStyle, { MainLayout } from '@styles/GlobalStyle';
 import HomePage from '@pages/HomePage';
 import InitRoomPage from '@pages/InitRoomPage';
 import MainPage from '@pages/MainPage';
-import TestPage from '@pages/TestPage';
 
 function Router() {
   return (
@@ -15,7 +14,6 @@ function Router() {
           <Route path="/" element={<HomePage />} />
           <Route path="/init-room" element={<InitRoomPage />} />
           <Route path="/room/:roomCode" element={<MainPage />} />
-          <Route path="/test" element={<TestPage />} />
         </Routes>
       </MainLayout>
     </BrowserRouter>
