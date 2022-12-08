02c1f0eb4f34e7f31162dfd6a404fbb6b18bc0c2
권유리 <99jiyusu@gmail.com>
2022-12-09 00:27:24 +0900

    feat: 후보 리스트 페이지에 후보 음식점 데이터 연동

--- client/src/components/RestaurantCandidateList/index.tsx
+++ client/src/components/RestaurantCandidateList/index.tsx
@@ -1,51 +1,71 @@
+import { useEffect, useState, useRef } from 'react';
+
 import RestaurantRow from '@components/RestaurantRow';
 import { RESTAURANT_LIST_TYPES } from '@constants/modal';
 import { useSocketStore } from '@store/socket';
 import { useVotedRestaurantListStore } from '@store/vote';
-import { useEffect } from 'react';
+
 import { Socket } from 'socket.io-client';
 import { CandidateListModalBox, CandidateListModalLayout } from './styles';
 
-interface CandidateType {
-  id: string;
-  name: string;
-  category: string;
-  lat: number;
-  lng: number;
-  like: number;
-  rating?: number;
-  thumbnailImage?: string;
-}
-
 interface PropsType {
-  candidateData: RestaurantType[];
+  restaurantData: RestaurantType[];
 }
 
-interface ResultType {
+interface VoteDataType {
+  restaurantId: string;
+  count: number;
+}
+interface VoteResultType {
   message: string;
-  data?: { candidateList: { restaurantId: string; count: number }[] };
-}
-class CandidateMap {
-  [key: string]: CandidateType;
+  data?: { candidateList: VoteDataType[] };
 }
 
-export function CandidateListModal({ candidateData }: PropsType) {
+export function CandidateListModal({ restaurantData }: PropsType) {
   const { socket } = useSocketStore((state) => state);
+  const voteList = useRef<VoteDataType[]>([]);
+  const [candidateData, setCandidateData] = useState<RestaurantType[]>([]); // 투표된 음식점의 정보 데이터
 
   if (!(socket instanceof Socket)) {
     throw new Error();
   }
 
+  const makeCandidateData = (candidateList: VoteDataType[]) => {
+    voteList.current = candidateList;
+    let temp: RestaurantType[] = [];
+
+    // voteList에 있는 후보 음식점들의 상세정보를 candidateData에 세팅
+    restaurantData.forEach((restaurantItem) => {
+      voteList.current.forEach((voteItem) => {
+        if (restaurantItem.id === voteItem.restaurantId) {
+          temp = [...temp, restaurantItem];
+        }
+      });
+    });
+
+    return temp;
+  };
+
   useEffect(() => {
-    // getVoteResult 필요
+    // 투표 결과 요청
+    socket.emit('getVoteResult');
+    socket.on('currentVoteResult', (result: VoteResultType) => {
+      if (!result.data) {
+        return;
+      }
+
+      setCandidateData(makeCandidateData(result.data?.candidateList));
+    });
+
+    // 투표 결과 업데이트 (다른 참여자가 투표하는 경우 발생)
+    socket.on('voteResultUpdate', (result: VoteResultType) => {
+      if (!result.data) {
+        return;
+      }
+
+      setCandidateData(makeCandidateData(result.data?.candidateList));
+    });
   }, []);
-
-  socket.on('voteResultUpdate', (result: ResultType) => {
-    console.log('투표 결과 업데이트됨');
-    console.log(result);
-  });
-
-  // 여기서 업데이트도 필요하지만, 요청하면 주는 것도 필요할 듯..?
 
   return (
     <CandidateListModalLayout>
