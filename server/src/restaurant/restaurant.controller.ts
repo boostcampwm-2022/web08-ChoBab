79eb4f1aae26a0faeb087517849c90f8457bb979
One-armed-boy <fanatic5500@gmail.com>
2022-12-10 03:03:39 +0900

    refactor: 구글 api를 사용하지 않음에 따라 수정되는 요소들에 대한 서버 로직 변경 적용
    - 음식점 이미지
    - 평점

--- server/src/restaurant/restaurant.controller.ts
+++ server/src/restaurant/restaurant.controller.ts
@@ -1,6 +1,4 @@
-import { RESTAURANT_RES } from '@common/response/restaurant';
-import { Controller, Get, Query } from '@nestjs/common';
-import { GetRestaurantDetailQueryDto } from './dto/get-restaurant-detail-query.dto';
+import { Controller } from '@nestjs/common';
 import { RestaurantService } from './restaurant.service';
 
 @Controller('restaurant')
@@ -8,17 +6,17 @@ export class RestaurantController {
   constructor(private readonly restaurantService: RestaurantService) {}
 
   // 당장 이를 사용하지 않기로
-  @Get()
-  async getRestaurantDetail(@Query() getRestaurantDetailDto: GetRestaurantDetailQueryDto) {
-    const { name, address, lat, lng, restaurantId: id } = getRestaurantDetailDto;
-    const { rating, priceLevel } = await this.restaurantService.getRestaurantDetail(
-      id,
-      address,
-      name,
-      lat,
-      lng
-    );
+  // @Get()
+  // async getRestaurantDetail(@Query() getRestaurantDetailDto: GetRestaurantDetailQueryDto) {
+  //   const { name, address, lat, lng, restaurantId: id } = getRestaurantDetailDto;
+  //   const { rating, priceLevel } = await this.restaurantService.getRestaurantDetail(
+  //     id,
+  //     address,
+  //     name,
+  //     lat,
+  //     lng
+  //   );
 
-    return RESTAURANT_RES.SUCCESS_SEARCH_RESTAURANT_DETAIL(rating, priceLevel);
-  }
+  //   return RESTAURANT_RES.SUCCESS_SEARCH_RESTAURANT_DETAIL(rating, priceLevel);
+  // }
 }
