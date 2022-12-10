1a6688802d5ee9e87aa111a6c1fcbc1351351fa1
CodeDiary18 <codediary18@gmail.com>
2022-12-10 18:11:09 +0900

    feat: 유효하지 않은 모임방, internal server error에 대한 route 추가

--- client/src/Router.tsx
+++ client/src/Router.tsx
@@ -16,6 +16,14 @@ function Router() {
           <Route path="/" element={<HomePage />} />
           <Route path="/init-room" element={<InitRoomPage />} />
           <Route path="/room/:roomCode" element={<MainPage />} />
+          <Route
+            path="/error/invalid-room"
+            element={<ErrorPage reason={ERROR.INVALID_ROOM_ID} />}
+          />
+          <Route
+            path="/error/internal-server"
+            element={<ErrorPage reason={ERROR.INTERNAL_SERVER_ERROR} />}
+          />
           <Route path="*" element={<ErrorPage reason={ERROR.NOT_FOUND_PAGE} />} />
         </Routes>
       </MainLayout>
