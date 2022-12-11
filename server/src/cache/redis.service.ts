31f9c43b0122f11b175a9d956a20595dbe19ad89
One-armed-boy <fanatic5500@gmail.com>
2022-12-11 16:30:33 +0900

    docs: redis.service 내 주석 작성

--- server/src/cache/redis.service.ts
+++ server/src/cache/redis.service.ts
@@ -23,6 +23,7 @@ interface JoinListType {
 export class RedisService {
   constructor(@Inject(CACHE_MANAGER) private readonly cacheManager: Cache) {}
 
+  // 각 도메인 별 Redis key를 반환하는 객체
   private readonly redisKey = {
     restaurantList: (roomCode: string) => `ROOM_CODE:${roomCode}:RESTAURANT_LIST`,
     candidateList: (roomCode: string) => `ROOM_CODE:${roomCode}:CANDIDATE_LIST`,
@@ -30,11 +31,13 @@ export class RedisService {
   };
 
   restaurantList = {
+    // 초기 모임 생성 시 음식점 정보를 Redis에 적재하는 로직
     setRestaurantListForRoom: async (roomCode: string, restaurantList: MergedRestaurantType[]) => {
       const restaurantListKey = this.redisKey.restaurantList(roomCode);
       await this.cacheManager.set(restaurantListKey, restaurantList);
     },
 
+    // 방 코드를 이용하여 해당 모임에서 사용하는 음식점 데이터를 반환
     getRestaurantListForRoom: async (roomCode: string) => {
       const restaurantListKey = this.redisKey.restaurantList(roomCode);
       const result = await this.cacheManager.get<MergedRestaurantType[]>(restaurantListKey);
@@ -43,6 +46,7 @@ export class RedisService {
   };
 
   candidateList = {
+    // 모임 생성 시 이후 사용할 빈 후보 음식점 리스트를 생성 후 Redis에 적재
     createEmptyCandidateListForRoom: async (
       roomCode: string,
       restaurantList: MergedRestaurantType[]
@@ -55,12 +59,14 @@ export class RedisService {
       await this.cacheManager.set(candidateListKey, candidateHash);
     },
 
+    // 방 코드를 이용하여 해당 모임의 후보 음식점 리스트를 반환
     getCandidateList: async (roomCode: string) => {
       const candidateListKey = this.redisKey.candidateList(roomCode);
       const candidateHash = this.cacheManager.get<CandidateHashType>(candidateListKey);
       return candidateHash;
     },
 
+    // 좋아요 투표를 수행하고 이에 대한 성공 여부를 반환
     likeCandidate: async (
       roomCode: string,
       userId: string,
@@ -70,7 +76,6 @@ export class RedisService {
       const candidateHash = await this.cacheManager.get<CandidateHashType>(candidateListKey);
       const restaurantVoteList = candidateHash[restaurantId];
       if (!restaurantVoteList) {
-        // 이후 throw로 예외처리 필요
         return false;
       }
       // 이미 투표한 식당에 대한 처리
@@ -82,6 +87,7 @@ export class RedisService {
       return true;
     },
 
+    // 투표 취소를 수행하고 이에 대한 성공 여부를 반환
     unlikeCandidate: async (
       roomCode: string,
       userId: string,
@@ -105,17 +111,20 @@ export class RedisService {
   };
 
   joinList = {
+    // 모임 생성 시 사용자들이 입장할 빈 리스트 생성
     createEmptyJoinListForRoom: async (roomCode: string) => {
       const joinListKey = this.redisKey.joinList(roomCode);
       await this.cacheManager.set(joinListKey, {});
     },
 
+    // 방 코드를 이용하여 현재 참여자 리스트를 반환
     getJoinList: async (roomCode: string) => {
       const joinListKey = this.redisKey.joinList(roomCode);
       const findJoinHash = await this.cacheManager.get<JoinListType>(joinListKey);
       return findJoinHash;
     },
 
+    // 사용자가 모임에 참여하는 로직
     addUserToJoinList: async (roomCode: string, user: UserType) => {
       const { userId } = user;
       const joinListKey = this.redisKey.joinList(roomCode);
@@ -129,9 +138,11 @@ export class RedisService {
       await this.cacheManager.set(joinListKey, { ...findJoinHash, ...addUserData });
     },
 
+    // 사용자를 모임에서 내보내는 로직
     delUserToJoinList: async (roomCode: string, userId: string) => {
       const joinListKey = this.redisKey.joinList(roomCode);
       const findJoinHash = await this.cacheManager.get<JoinListType>(joinListKey);
+      // 해당 방에 이미 접속해있지 않을 때
       if (!findJoinHash[userId]) {
         return;
       }
