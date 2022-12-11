c5225cda65b0e01dfa5cb24aa6019e601d3244ad
권유리 <99jiyusu@gmail.com>
2022-12-12 04:39:05 +0900

    feat: react-window 설치 및 적용

--- client/src/components/RestaurantFilteredList/index.tsx
+++ client/src/components/RestaurantFilteredList/index.tsx
@@ -1,4 +1,15 @@
 import {
+  Component,
+  ComponentType,
+  CSSProperties,
+  Ref,
+  Key,
+  FunctionComponent,
+  ComponentClass,
+} from 'react';
+
+import { VariableSizeList } from 'react-window';
+import {
   useRestaurantDetailLayerStatusStore,
   useSelectedRestaurantDataStore,
   useSelectedCategoryStore,
@@ -33,28 +44,46 @@ function RestaurantFiltered({ restaurantData }: PropsType) {
     )
     .slice(0, 20);
 
+  const getItemSize = (index: any) => {
+    // 아이템 행 높이 계산 필요
+    return 200;
+  };
+
+  // eslint-disable-next-line react/no-unstable-nested-components
+  function Row({ index, style }: { index: number; style: CSSProperties }) {
+    const restaurant = restaurantData[index];
+    return (
+      <div style={style}>
+        <RestaurantFilteredItem
+          onClick={() => {
+            updateRestaurantDetailLayerStatus(RESTAURANT_DETAIL_TYPES.show);
+            updateSelectedRestaurantData(restaurant);
+          }}
+          key={restaurant.id}
+        >
+          <RestaurantRow
+            restaurant={restaurant}
+            restaurantListType={RESTAURANT_LIST_TYPES.filtered}
+          />
+        </RestaurantFilteredItem>
+      </div>
+    );
+  }
+
   return (
     <RestaurantFilteredBox>
       {!restaurantFilteredList.length ? (
         <EmptyListPlaceholder />
       ) : (
         <RestaurantFilteredList>
-          {restaurantFilteredList.map((restaurant) => {
-            return (
-              <RestaurantFilteredItem
-                onClick={() => {
-                  updateRestaurantDetailLayerStatus(RESTAURANT_DETAIL_TYPES.show);
-                  updateSelectedRestaurantData(restaurant);
-                }}
-                key={restaurant.id}
-              >
-                <RestaurantRow
-                  restaurant={restaurant}
-                  restaurantListType={RESTAURANT_LIST_TYPES.filtered}
-                />
-              </RestaurantFilteredItem>
-            );
-          })}
+          <VariableSizeList
+            height={400}
+            width={300}
+            itemCount={restaurantData.length}
+            itemSize={getItemSize}
+          >
+            {Row}
+          </VariableSizeList>
         </RestaurantFilteredList>
       )}
     </RestaurantFilteredBox>
