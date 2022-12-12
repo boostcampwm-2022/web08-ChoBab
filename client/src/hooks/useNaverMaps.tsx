7f6d44ed6a30dab13ba1c0ea141c6db0cbdb9dc8
One-armed-boy <fanatic5500@gmail.com>
2022-12-12 15:50:59 +0900

    refactor: 최소 줌 레벨 상수로 분리

--- client/src/hooks/useNaverMaps.tsx
+++ client/src/hooks/useNaverMaps.tsx
@@ -1,5 +1,5 @@
 import { MutableRefObject, RefObject, useEffect, useRef } from 'react';
-import { NAVER_LAT, NAVER_LNG } from '@constants/map';
+import { MAIN_MAPS_MIN_ZOOM_LEVEL, NAVER_LAT, NAVER_LNG } from '@constants/map';
 
 export const useNaverMaps = (): [
   MutableRefObject<naver.maps.Map | null>,
@@ -17,7 +17,7 @@ export const useNaverMaps = (): [
       center: new naver.maps.LatLng(NAVER_LAT, NAVER_LNG),
       zoom: 16,
       // 7로 잡아도 대한민국 전역을 커버 가능
-      minZoom:7,
+      minZoom: MAIN_MAPS_MIN_ZOOM_LEVEL,
     });
   }, []);
 
