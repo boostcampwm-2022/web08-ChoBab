e83e54301723ce6649642d6fcd9ea4befbf00bbf
jeong57281 <jeong5728@gmail.com>
2022-12-13 14:19:51 +0900

    fix: 상세보기 화면에서 이벤트 버블링 일으키는 문제 해결

--- client/src/components/RestaurantDetailLayer/index.tsx
+++ client/src/components/RestaurantDetailLayer/index.tsx
@@ -15,6 +15,14 @@ function RestaurantDetailLayer() {
           animate={{ opacity: 1, x: 0 }}
           exit={{ opacity: 0, x: 999 }}
           transition={{ duration: 1 }}
+          /**
+           * 식당 요약정보 -> 식당 상세정보 경로로 레이어가 열렸을 때
+           * 다시 돌아갔을 경우에도 식당 요약정보가 닫히지 않도록 하기 위함.
+           * window에 등록한 이벤트 리스너를 실행시키지 않는다.
+           */
+          onClick={(event) => {
+            event.stopPropagation();
+          }}
         >
           <RestaurantDetailModal
             updateRestaurantDetailLayerStatus={updateRestaurantDetailLayerStatus}
